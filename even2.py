2de211ca812f8993949ea4b6f1fea109fbc625b1
Adam Nambiar <adamnambiar@Adams-MBP.home>
2022-05-21 12:00:46 -0400

    May 21, 2022: Even number using range and loop, for and if statment

---
+++ even2.py
@@ -0,0 +1,16 @@
+import time
+
+
+st = time.time()
+
+numbers = range(1000000001)
+for y in numbers:
+	#print (y%2)
+	remainder=y%2
+	if remainder==0:
+		print (y)
+
+et = time.time()
+elapsed_time = et - st
+print ('Execution time:', elapsed_time, 'seconds')
+
